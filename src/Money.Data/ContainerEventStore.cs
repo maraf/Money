2ae238d3c1dbf1b358a8e9296a12422b709acae3
maraf <mara@neptuo.com>
2016-11-19 09:39:57 +0100

    Add project for data implementation.

---
+++ src/Money.Data/ContainerEventStore.cs
@@ -0,0 +1,28 @@
+﻿using Neptuo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neptuo.Models.Keys;
+
+namespace Money.Data
+{
+    public class ContainerEventStore : IEventStore
+    {
+        public IEnumerable<EventModel> Get(IKey aggregateKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EventModel> Get(IKey aggregateKey, int version)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IEnumerable<EventModel> events)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

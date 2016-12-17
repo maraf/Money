aff0f92888da0993385005cca899cb6344ed894c
maraf <mara@neptuo.com>
2016-12-17 11:25:07 +0100

    Basic infrastructure for upgrading.

---
+++ src/Neptuo/Migrations/IUpgradeContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Migrations
+{
+    /// <summary>
+    /// A context of upgrade operation.
+    /// </summary>
+    public interface IUpgradeContext
+    {
+        /// <summary>
+        /// Sets a number of steps needed to execute.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns>Self (for fluency).</returns>
+        IUpgradeContext TotalSteps(int count);
+
+        /// <summary>
+        /// Sets a currently executing step.
+        /// </summary>
+        /// <param name="index">An index of executing step.</param>
+        /// <param name="caption">A step caption.</param>
+        /// <returns>Self (for fluency).</returns>
+        IUpgradeContext StartingStep(int index, string caption);
+    }
+}
---
+++ src/Neptuo/Migrations/IUpgradeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Migrations
+{
+    public interface IUpgradeService
+    {
+        /// <summary>
+        /// Returns a <c>true</c> if upgrade is required.
+        /// </summary>
+        /// <returns><c>true</c> if upgrade is required; <c>false</c> otherwise.</returns>
+        bool IsRequired();
+
+        /// <summary>
+        /// Executes required upgrades.
+        /// </summary>
+        /// <param name="context">A context of upgrade.</param>
+        /// <returns>Continuation task.</returns>
+        Task UpgradeAsync(IUpgradeContext context);
+    }
+}

d7a1c22813f6cc78f779bf0b2b5491ae00e3e075
maraf <mara@neptuo.com>
2017-01-13 07:00:28 +0100

    Fix binding a value from DatePicker to ViewModel.

---
+++ src/Money.UI/Views/Converters/DateTimeOffsetToDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Money.Views.Converters
+{
+    public class DateTimeOffsetToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new DateTimeOffset((DateTime)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            DateTimeOffset offset = (DateTimeOffset)value;
+            return offset.Date;
+        }
+    }
+}

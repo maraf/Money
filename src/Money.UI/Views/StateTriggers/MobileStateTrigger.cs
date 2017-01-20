91cad9935b0220d11547972916976b3db96e19a2
Marek Fišera <mara@neptuo.com>
2017-01-20 21:10:23 +0100

    Main menu items moved to bottom on mobile. In narrow view, menu icon is replaced with app icon.

---
+++ src/Money.UI/Views/StateTriggers/MobileStateTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+
+namespace Money.Views.StateTriggers
+{
+    public class MobileStateTrigger : StateTriggerBase
+    {
+        public MobileStateTrigger()
+        {
+            string deviceFamily = AnalyticsInfo.VersionInfo.DeviceFamily;
+            SetActive(deviceFamily == "Windows.Mobile");
+        }
+    }
+}

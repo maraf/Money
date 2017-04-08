91b399b32082fb8fb8a43b74f7aa25e838bac5f1
Marek Fišera <mara@neptuo.com>
2017-04-08 16:42:39 +0200

    Content dialog for adding currency exchange rate.

---
+++ src/Money.UI/Views/Dialogs/CurrencyExchangeRate.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Money.Services.Models.Queries;
+using Neptuo;
+using Neptuo.Queries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Money.Views.Dialogs
+{
+    public sealed partial class CurrencyExchangeRate : ContentDialog
+    {
+        private readonly IQueryDispatcher queryDispatcher;
+
+        public string SourceCurrency
+        {
+            get { return (string)GetValue(SourceCurrencyProperty); }
+            set { SetValue(SourceCurrencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceCurrencyProperty = DependencyProperty.Register(
+            "SourceCurrency", 
+            typeof(string), 
+            typeof(CurrencyExchangeRate), 
+            new PropertyMetadata(null)
+        );
+
+        public string TargetCurrency
+        {
+            get { return (string)GetValue(TargetCurrencyProperty); }
+            set { SetValue(TargetCurrencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetCurrencyProperty = DependencyProperty.Register(
+            "TargetCurrency", 
+            typeof(string), 
+            typeof(CurrencyExchangeRate), 
+            new PropertyMetadata(null)
+        );
+
+        public double Rate
+        {
+            get { return (double)GetValue(RateProperty); }
+            set { SetValue(RateProperty, value); }
+        }
+
+        public static readonly DependencyProperty RateProperty = DependencyProperty.Register(
+            "Rate", 
+            typeof(double), 
+            typeof(CurrencyExchangeRate), 
+            new PropertyMetadata(1)
+        );
+
+        public CurrencyExchangeRate(IQueryDispatcher queryDispatcher)
+        {
+            Ensure.NotNull(queryDispatcher, "queryDispatcher");
+            this.queryDispatcher = queryDispatcher;
+
+            InitializeComponent();
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            List<string> currencies = await queryDispatcher.QueryAsync(new ListAllCurrency());
+            cbxCurrency.ItemsSource = currencies;
+        }
+
+        private void tbxRate_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                e.Handled = true;
+            }
+            else if (e.Key == VirtualKey.Escape)
+            {
+                if (sender == tbxRate)
+                {
+                    if (tbxRate.Text == "1")
+                    {
+                        Hide();
+                        return;
+                    }
+
+                    tbxRate.Text = "1";
+                    tbxRate.SelectAll();
+                    e.Handled = true;
+                }
+            }
+        }
+
+    }
+}

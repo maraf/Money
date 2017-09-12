443befd0dada8ecf01707cf758698cee053c4f2c
Marek Fišera <mara@neptuo.com>
2017-09-12 22:03:02 +0200

    Reimplementation of currency list design.

---
+++ src/Money.UI/Views/Converters/ExchangeRateListToStringConverter.cs
@@ -0,0 +1,38 @@
+﻿using Money.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Money.Views.Converters
+{
+    public class ExchangeRateListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            IReadOnlyList<ExchangeRateModel> models = (IReadOnlyList<ExchangeRateModel>)value;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < models.Count; i++)
+            {
+                if (result.Length > 0)
+                {
+                    if (i == models.Count - 1)
+                        result.Append(" and ");
+                    else
+                        result.Append(", ");
+                }
+
+                result.Append(models[i].SourceCurrency);
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

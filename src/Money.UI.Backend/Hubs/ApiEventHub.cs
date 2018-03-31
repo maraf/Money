f94a0e889cb14e8a2c1e12b73f2464337a554a88
Marek Fišera <mara@neptuo.com>
2018-03-31 16:59:41 +0200

    #117 - Editing, deleting and changing default currency. Syncing domain currency events to client.

--- src/Money.UI.Backend/Hubs/ApiEventHub.cs
+++ src/Money.UI.Backend/Hubs/ApiEventHub.cs
@@ -15,7 +15,9 @@ using System.Threading.Tasks;
 
 namespace Money.Hubs
 {
-    public class ApiEventHub : Hub, IEventHandler<CategoryCreated>, IEventHandler<CategoryDeleted>, IEventHandler<CategoryRenamed>, IEventHandler<CategoryDescriptionChanged>, IEventHandler<CategoryIconChanged>, IEventHandler<CategoryColorChanged>
+    public class ApiEventHub : Hub, 
+        IEventHandler<CategoryCreated>, IEventHandler<CategoryDeleted>, IEventHandler<CategoryRenamed>, IEventHandler<CategoryDescriptionChanged>, IEventHandler<CategoryIconChanged>, IEventHandler<CategoryColorChanged>,
+        IEventHandler<CurrencyCreated>, IEventHandler<CurrencyDeleted>, IEventHandler<CurrencyDefaultChanged>, IEventHandler<CurrencySymbolChanged>
     {
         private readonly Formatters formatters;
 
@@ -47,5 +49,10 @@ namespace Money.Hubs
         Task IEventHandler<CategoryDescriptionChanged>.HandleAsync(CategoryDescriptionChanged payload) => RaiseEvent(payload);
         Task IEventHandler<CategoryIconChanged>.HandleAsync(CategoryIconChanged payload) => RaiseEvent(payload);
         Task IEventHandler<CategoryColorChanged>.HandleAsync(CategoryColorChanged payload) => RaiseEvent(payload);
+
+        Task IEventHandler<CurrencyCreated>.HandleAsync(CurrencyCreated payload) => RaiseEvent(payload);
+        Task IEventHandler<CurrencyDeleted>.HandleAsync(CurrencyDeleted payload) => RaiseEvent(payload);
+        Task IEventHandler<CurrencyDefaultChanged>.HandleAsync(CurrencyDefaultChanged payload) => RaiseEvent(payload);
+        Task IEventHandler<CurrencySymbolChanged>.HandleAsync(CurrencySymbolChanged payload) => RaiseEvent(payload);
     }
 }

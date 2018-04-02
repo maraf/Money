911400a3900353516f77f2a9a7c6e1f32224fc7a
Marek Fišera <mara@neptuo.com>
2018-04-02 10:56:09 +0200

    #109 - Extensions for automatic UserKey processing in commnad handlers.

---
+++ src/Money/Commands/Handlers/AggregateRootCommandExecutorExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Money.Events;
+using Neptuo;
+using Neptuo.Commands.Handlers;
+using Neptuo.Events;
+using Neptuo.Models.Domains;
+using Neptuo.Models.Keys;
+using Neptuo.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Commands.Handlers
+{
+    internal static class AggregateRootCommandExecutorExtensions
+    {
+        public static Task Execute<T, TRepository>(this AggregateRootCommandExecutor<T, TRepository> executor, IKey aggregateKey, Envelope envelope, Action<T, IKey> handler)
+            where T : AggregateRoot
+            where TRepository : IRepository<T, IKey>
+        {
+            return executor.Execute(aggregateKey, model =>
+            {
+                IKey userKey;
+                if (envelope.Metadata.TryGet("UserId", out string userId))
+                    userKey = StringKey.Create(userId, "User");
+                else
+                    userKey = StringKey.Empty("User");
+
+                handler(model, userKey);
+
+                foreach (IEvent item in model.Events)
+                {
+                    if (item is UserEvent payload)
+                        payload.UserKey = userKey;
+                }
+            });
+        }
+
+        public static Task Execute<T, TRepository>(this AggregateRootCommandExecutor<T, TRepository> executor, IKey aggregateKey, Envelope envelope, Action<T> handler)
+            where T : AggregateRoot
+            where TRepository : IRepository<T, IKey>
+        {
+            return Execute(executor, aggregateKey, envelope, (model, userKey) => handler(model));
+        }
+
+    }
+}

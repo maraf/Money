28fb91c0c920f6d6df848bc07337652180a434e6
Marek Fišera <mara@neptuo.com>
2018-04-06 10:20:35 +0200

    Generic bootstrap alert component.

---
+++ src/Money.UI.Blazor/Components/Bootstrap/AlertMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Components.Bootstrap
+{
+    public enum AlertMode
+    {
+        Success,
+        Info,
+        Warning,
+        Error
+    }
+}

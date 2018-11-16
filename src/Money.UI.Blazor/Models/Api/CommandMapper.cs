fac76f59a3992e75ac7ff83659d5050b458f5218
Marek Fišera <mara@neptuo.com>
2018-11-16 21:49:24 +0100

    #188 - All commands has URL.

--- src/Money.UI.Blazor/Models/Api/CommandMapper.cs
+++ src/Money.UI.Blazor/Models/Api/CommandMapper.cs
@@ -15,7 +15,21 @@ namespace Money.Models.Api
             Add<ChangeOutcomeAmount>("outcome-change-amount");
             Add<ChangeOutcomeDescription>("outcome-change-description");
             Add<ChangeOutcomeWhen>("outcome-change-when");
-            Add<DeleteOutcome>("outcome-change-delete");
+            Add<DeleteOutcome>("outcome-delete");
+
+            Add<CreateCategory>("category-create");
+            Add<ChangeCategoryColor>("category-change-color");
+            Add<ChangeCategoryIcon>("category-change-icon");
+            Add<ChangeCategoryDescription>("category-change-description");
+            Add<RenameCategory>("category-rename");
+            Add<DeleteCategory>("category-delete");
+
+            Add<CreateCurrency>("currency-create");
+            Add<ChangeCurrencySymbol>("currency-change-symbol");
+            Add<SetCurrencyAsDefault>("currency-set-as-default");
+            Add<SetExchangeRate>("currency-exchangerate-set");
+            Add<RemoveExchangeRate>("currency-exchangerate-remove");
+            Add<DeleteCurrency>("currency-delete");
         }
     }
 }

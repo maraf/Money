b9bb966cde4cc12b846580b8419ea71a4e4e48e3
Marek Fišera <mara@neptuo.com>
2018-11-16 21:35:50 +0100

    #191 - Main menu fully responsive.

---
+++ src/Money.UI.Blazor/Components/MainMenu.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using Money.Services;
+using Neptuo.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Components
+{
+    public class MainMenuBase : BlazorComponent, IDisposable
+    {
+        [Inject]
+        internal Navigator Navigator {get;set;}
+
+        [Inject]
+        internal ILog<MainMenuBase> Log { get; set; }
+
+        protected bool IsMainMenuVisible { get; set; } = false;
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+            Navigator.LocationChanged += OnLocationChanged;
+        }
+
+        public void Dispose()
+        {
+            Navigator.LocationChanged -= OnLocationChanged;
+        }
+
+        private void UpdateMainMenuVisible(bool isVisible)
+        {
+            if (IsMainMenuVisible != isVisible)
+            {
+                Log.Debug($"IsMainMenuVisible: Previous='{IsMainMenuVisible}', New='{isVisible}'.");
+                IsMainMenuVisible = isVisible;
+                StateHasChanged();
+            }
+        }
+
+        private void OnLocationChanged(string url)
+            => UpdateMainMenuVisible(false);
+
+        protected void OnMainMenuToggleClick()
+            => UpdateMainMenuVisible(!IsMainMenuVisible);
+    }
+}

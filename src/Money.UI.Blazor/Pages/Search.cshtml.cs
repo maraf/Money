0ad3b4c8cd5d02d41cbfc1851b3dff50ab7917e3
Marek Fišera <mara@neptuo.com>
2018-11-20 17:34:22 +0100

    #186 - Empty page for searching.

---
+++ src/Money.UI.Blazor/Pages/Search.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Pages
+{
+    public class SearchBase : BlazorComponent
+    {
+        
+    }
+}

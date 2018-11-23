1a3ed42f53f3bdb017812563400c9cd09a5cfe4b
Marek Fišera <mara@neptuo.com>
2018-11-23 19:44:43 +0100

    A different view on overview.

---
+++ src/Money.UI.Blazor/Pages/OverviewSortType.cs
@@ -0,0 +1,40 @@
+﻿using Money.Models.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Pages
+{
+    /// <summary>
+    /// An enumeration of supported sorting of overview items.
+    /// </summary>
+    public enum OverviewSortType
+    {
+        /// <summary>
+        /// Sort items by amount.
+        /// </summary>
+        [DisplayName("By Amount")]
+        [DefaultValue(SortDirection.Descending)]
+        ByAmount,
+
+        /// <summary>
+        /// Sort items by category name.
+        /// </summary>
+        [DisplayName("By Category")]
+        ByCategory,
+
+        /// <summary>
+        /// Sort items by description.
+        /// </summary>
+        [DisplayName("By Description")]
+        ByDescription,
+
+        /// <summary>
+        /// Sort items by creation date.
+        /// </summary>
+        [DisplayName("By Creation Date")]
+        ByWhen
+    }
+}

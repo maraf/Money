c5b7ba1ef163715cf295e7c33bed9db00c24e2dc
Marek Fišera <mara@neptuo.com>
2019-12-28 21:28:49 +0100

    #179 - Refactored universal local storage.

--- src/Money.UI.Blazor/Services/CurrencyStorage.cs
+++ src/Money.UI.Blazor/Services/CurrencyStorage.cs
@@ -12,38 +12,10 @@ using System.Threading.Tasks;
 
 namespace Money.Services
 {
-    internal class CurrencyStorage
+    public class CurrencyStorage : JsonLocalStorage<List<CurrencyModel>>
     {
-        private readonly Json json;
-        private readonly IJSRuntime jsRuntime;
-        private readonly ILog<CurrencyStorage> log;
-
-        public CurrencyStorage(Json json, IJSRuntime jsRuntime, ILog<CurrencyStorage> log)
-        {
-            Ensure.NotNull(json, "json");
-            Ensure.NotNull(jsRuntime, "jsRuntime");
-            Ensure.NotNull(log, "log");
-            this.json = json;
-            this.jsRuntime = jsRuntime;
-            this.log = log;
-        }
-
-        public async Task<List<CurrencyModel>> LoadAsync()
-        {
-            string raw = await jsRuntime.InvokeAsync<string>("window.localStorage.getItem", "currencies");
-            log.Debug($"Load currencies: '{raw}'.");
-            if (String.IsNullOrEmpty(raw))
-                return null;
-
-            List<CurrencyModel> models = json.Deserialize<List<CurrencyModel>>(raw);
-            return models;
-        }
-
-        public async Task SaveAsync(IReadOnlyCollection<CurrencyModel> models)
-        {
-            string raw = json.Serialize(models);
-            log.Debug($"Save currencies: '{raw}'.");
-            await jsRuntime.InvokeVoidAsync("window.localStorage.setItem", "currencies", raw);
-        }
+        public CurrencyStorage(Json json, IJSRuntime jsRuntime, ILogFactory logFactory)
+            : base(json, jsRuntime, logFactory, "currencies")
+        { }
     }
 }
---
+++ src/Money.UI.Blazor/Services/JsonLocalStorage.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.JSInterop;
+using Money.Models;
+using Neptuo;
+using Neptuo.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Services
+{
+    public abstract class JsonLocalStorage<T>
+        where T : class
+    {
+        private readonly Json json;
+        private readonly IJSRuntime jsRuntime;
+        private readonly ILog log;
+        private readonly string key;
+
+        public JsonLocalStorage(Json json, IJSRuntime jsRuntime, ILogFactory logFactory, string key)
+        {
+            Ensure.NotNull(json, "json");
+            Ensure.NotNull(jsRuntime, "jsRuntime");
+            Ensure.NotNull(logFactory, "logFactory");
+            Ensure.NotNullOrEmpty(key, "key");
+            this.json = json;
+            this.jsRuntime = jsRuntime;
+            this.log = logFactory.Scope(GetType().Name);
+            this.key = key;
+        }
+
+        public async Task<T> LoadAsync()
+        {
+            try
+            {
+                string raw = await jsRuntime.InvokeAsync<string>("window.localStorage.getItem", key);
+                log.Debug($"Loaded '{raw}'.");
+                if (String.IsNullOrEmpty(raw))
+                    return null;
+
+                T model = json.Deserialize<T>(raw);
+                return model;
+            }
+            catch (Exception e)
+            {
+                log.Fatal(e);
+                return null;
+            }
+        }
+
+        public async Task SaveAsync(T model)
+        {
+            string raw = json.Serialize(model);
+            await jsRuntime.InvokeVoidAsync("window.localStorage.setItem", key, raw);
+            log.Debug($"Saved '{raw}'.");
+        }
+    }
+}

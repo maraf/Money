55b12bf3d59251d75299d265036647db662e3655
Marek Fišera <mara@neptuo.com>
2020-01-02 18:29:30 +0100

    #179 - Base page for local expense bag.

---
+++ src/Money.UI.Blazor/Pages/ExpenseBag.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Money.Commands;
+using Money.Components;
+using Money.Models.Loading;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Pages
+{
+    public partial class ExpenseBag
+    {
+        protected List<CreateOutcome> Models { get; } = new List<CreateOutcome>();
+        protected LoadingContext Loading { get; } = new LoadingContext();
+
+        protected ModalDialog CreateModal { get; set; }
+
+        protected void CreateNewExpense()
+        {
+            CreateModal.Show();
+            StateHasChanged();
+        }
+    }
+}

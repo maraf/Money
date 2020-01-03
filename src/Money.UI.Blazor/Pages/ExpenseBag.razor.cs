7d9131c64b6f530273cb125d8172caaf55d0af21
Marek Fišera <mara@neptuo.com>
2020-01-03 21:35:14 +0100

    #179 - Fire event locally stored expense has been created.

--- src/Money.UI.Blazor/Pages/ExpenseBag.razor.cs
+++ src/Money.UI.Blazor/Pages/ExpenseBag.razor.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Money.Commands;
 using Money.Components;
+using Money.Events;
 using Money.Models.Loading;
+using Neptuo.Events;
+using Neptuo.Events.Handlers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,16 +14,49 @@ using System.Threading.Tasks;
 
 namespace Money.Pages
 {
-    public partial class ExpenseBag
+    public partial class ExpenseBag : IDisposable, IEventHandler<CreateExpenseStoredLocally>
     {
+        [Inject]
+        internal IEventHandlerCollection EventHandlers { get; set; }
+
+        [Inject]
+        internal CreateExpenseStorage Storage { get; set; }
+
         protected List<CreateOutcome> Models { get; } = new List<CreateOutcome>();
         protected LoadingContext Loading { get; } = new LoadingContext();
 
         protected ModalDialog CreateModal { get; set; }
 
+        protected async override Task OnInitializedAsync()
+        {
+            EventHandlers.Add<CreateExpenseStoredLocally>(this);
+
+            await base.OnInitializedAsync();
+            await LoadAsync();
+        }
+
+        public void Dispose()
+        {
+            EventHandlers.Remove<CreateExpenseStoredLocally>(this);
+        }
+
+        private async Task LoadAsync()
+        {
+            Models.Clear();
+            var models = await Storage.LoadAsync();
+            if (models != null)
+                Models.AddRange(models);
+        }
+
         protected void CreateNewExpense()
         {
             CreateModal.Show();
+            StateHasChanged();
+        }
+
+        async Task IEventHandler<CreateExpenseStoredLocally>.HandleAsync(CreateExpenseStoredLocally payload)
+        {
+            await LoadAsync();
             StateHasChanged();
         }
     }
--- src/Money.UI.Blazor/Services/Commands/CommandStorage.cs
+++ src/Money.UI.Blazor/Services/Commands/CommandStorage.cs
@@ -1,4 +1,6 @@
-﻿using Neptuo;
+﻿using Money.Events;
+using Neptuo;
+using Neptuo.Events;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,11 +13,14 @@ namespace Money.Commands
     public class CommandStorage
     {
         private readonly CreateExpenseStorage expenseStorage;
+        private readonly IEventDispatcher eventDispatcher;
 
-        public CommandStorage(CreateExpenseStorage expenseStorage)
+        public CommandStorage(CreateExpenseStorage expenseStorage, IEventDispatcher eventDispatcher)
         {
             Ensure.NotNull(expenseStorage, "expenseStorage");
+            Ensure.NotNull(eventDispatcher, "eventDispatcher");
             this.expenseStorage = expenseStorage;
+            this.eventDispatcher = eventDispatcher;
         }
 
         public async Task StoreAsync<TCommand>(TCommand command)
@@ -25,6 +30,7 @@ namespace Money.Commands
                 var expenses = await expenseStorage.LoadAsync() ?? new List<CreateOutcome>();
                 expenses.Add(expense);
                 await expenseStorage.SaveAsync(expenses);
+                await eventDispatcher.PublishAsync(new CreateExpenseStoredLocally());
             }
             else
             {

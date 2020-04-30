cddd56e73ee0541a3ad5c0933ba90c525638802b
Marek Fišera <mara@neptuo.com>
2020-04-30 20:15:34 +0200

    #266 - User ServerConnectionState in OfflineCommandDispatcher.

--- src/Money.UI.Blazor/Services/Commands/OfflineCommandDispatcher.cs
+++ src/Money.UI.Blazor/Services/Commands/OfflineCommandDispatcher.cs
@@ -13,22 +13,23 @@ namespace Money.Commands
     internal class OfflineCommandDispatcher : ICommandDispatcher
     {
         private readonly HttpCommandDispatcher inner;
-        private readonly NetworkState network;
+        private readonly ServerConnectionState serverConnection;
+        private readonly IApiHubState hubState;
         private readonly CommandStorage storage;
 
-        public OfflineCommandDispatcher(HttpCommandDispatcher inner, NetworkState network, CommandStorage storage)
+        public OfflineCommandDispatcher(HttpCommandDispatcher inner, ServerConnectionState serverConnection, CommandStorage storage)
         {
             Ensure.NotNull(inner, "inner");
-            Ensure.NotNull(network, "network");
+            Ensure.NotNull(serverConnection, "serverConnection");
             Ensure.NotNull(storage, "expenseStorage");
             this.inner = inner;
-            this.network = network;
+            this.serverConnection = serverConnection;
             this.storage = storage;
         }
 
         public Task HandleAsync<TCommand>(TCommand command)
         {
-            if (network.IsOnline)
+            if (serverConnection.IsAvailable)
                 return inner.HandleAsync(command);
             else
                 return storage.StoreAsync(command);

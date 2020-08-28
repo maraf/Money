82f1fbeb3d8416e0d59396c5e30abc14952c6471
Marek Fišera <mara@neptuo.com>
2020-08-28 11:19:16 +0200

    #299 - Add initial migrations.

---
+++ src/Money.EventSourcing.EntityFrameworkCore/Migrations/20200828082525_Initial.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Money.EntityFrameworkCore.Migrations
+{
+    public partial class Initial : MigrationWithSchema<EventSourcingContext>
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: Schema.Name);
+
+            migrationBuilder.CreateTable(
+                name: "Command",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommandID = table.Column<Guid>(nullable: false),
+                    CommandType = table.Column<string>(nullable: true),
+                    Payload = table.Column<string>(nullable: true),
+                    RaisedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Command", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<Guid>(nullable: false),
+                    EventType = table.Column<string>(nullable: true),
+                    AggregateID = table.Column<Guid>(nullable: false),
+                    AggregateType = table.Column<string>(nullable: true),
+                    Payload = table.Column<string>(nullable: true),
+                    Version = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnPublishedCommand",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommandID = table.Column<int>(nullable: false),
+                    IsHandled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnPublishedCommand", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_UnPublishedCommand_Command_CommandID",
+                        column: x => x.CommandID,
+                        principalSchema: Schema.Name,
+                        principalTable: "Command",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnPublishedEvent",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnPublishedEvent", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_UnPublishedEvent_Event_EventID",
+                        column: x => x.EventID,
+                        principalSchema: Schema.Name,
+                        principalTable: "Event",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventPublishedToHandler",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<int>(nullable: false),
+                    HandlerIdentifier = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventPublishedToHandler", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_EventPublishedToHandler_UnPublishedEvent_EventID",
+                        column: x => x.EventID,
+                        principalSchema: Schema.Name,
+                        principalTable: "UnPublishedEvent",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventPublishedToHandler_EventID",
+                schema: Schema.Name,
+                table: "EventPublishedToHandler",
+                column: "EventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnPublishedCommand_CommandID",
+                schema: Schema.Name,
+                table: "UnPublishedCommand",
+                column: "CommandID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnPublishedEvent_EventID",
+                schema: Schema.Name,
+                table: "UnPublishedEvent",
+                column: "EventID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventPublishedToHandler",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "UnPublishedCommand",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "UnPublishedEvent",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "Command",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "Event",
+                schema: Schema.Name);
+        }
+    }
+}
---
+++ src/Money.Models.EntityFrameworkCore/Migrations/20200828082839_Initial.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Money.EntityFrameworkCore.Migrations;
+
+namespace Money.Models.Migrations
+{
+    public partial class Initial : MigrationWithSchema<ReadModelContext>
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: Schema.Name);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Icon = table.Column<string>(nullable: true),
+                    ColorA = table.Column<byte>(nullable: false),
+                    ColorR = table.Column<byte>(nullable: false),
+                    ColorG = table.Column<byte>(nullable: false),
+                    ColorB = table.Column<byte>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    UserId = table.Column<string>(maxLength: 36, nullable: false),
+                    UniqueCode = table.Column<string>(maxLength: 16, nullable: false),
+                    Symbol = table.Column<string>(nullable: true),
+                    IsDefault = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => new { x.UserId, x.UniqueCode });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    TargetCurrency = table.Column<string>(nullable: true),
+                    SourceCurrency = table.Column<string>(nullable: true),
+                    Rate = table.Column<double>(nullable: false),
+                    ValidFrom = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Outcomes",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Currency = table.Column<string>(nullable: true),
+                    When = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outcomes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutcomeCategories",
+                schema: Schema.Name,
+                columns: table => new
+                {
+                    OutcomeId = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutcomeCategories", x => new { x.OutcomeId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_OutcomeCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalSchema: Schema.Name,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OutcomeCategories_Outcomes_OutcomeId",
+                        column: x => x.OutcomeId,
+                        principalSchema: Schema.Name,
+                        principalTable: "Outcomes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutcomeCategories_CategoryId",
+                schema: Schema.Name,
+                table: "OutcomeCategories",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currencies",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "ExchangeRates",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "OutcomeCategories",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: Schema.Name);
+
+            migrationBuilder.DropTable(
+                name: "Outcomes",
+                schema: Schema.Name);
+        }
+    }
+}

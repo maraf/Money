0b11d40553e5a389cff242f26d664d0920d6259b
Marek Fišera <mara@neptuo.com>
2020-08-31 08:49:34 +0200

    #299 - Display migration progress messages. Fix identity insert. Fix composite key entity contains testing.

--- src/Money.DbContextDataMigrator/Program.cs
+++ src/Money.DbContextDataMigrator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Money.EntityFrameworkCore;
+using Money.EntityFrameworkCore.Migrations;
 using Money.Models;
 using System;
 using System.Collections.Generic;
@@ -24,15 +25,15 @@ namespace Money
             await MigrateReadModelAsync(readModelSource, readModelTarget);
         }
 
-        private static Task CopyDbSetAsync<TContext, TEntity>(TContext sourceContext, TContext targetContext, Func<TContext, DbSet<TEntity>> dbSetGetter, Action<TEntity> entityHandler = null, params string[] includes)
+        private static Task CopyDbSetAsync<TContext, TEntity>(TContext sourceContext, TContext targetContext, Func<TContext, DbSet<TEntity>> dbSetGetter, Action<TEntity> entityHandler = null, Func<TEntity, Task<bool>> containsHandler = null, params string[] includes)
             where TEntity : class
         {
             var source = dbSetGetter(sourceContext);
             var target = dbSetGetter(targetContext);
-            return CopyDbSetAsync(source, target, entityHandler, includes);
+            return CopyDbSetAsync(source, target, entityHandler, containsHandler, includes);
         }
 
-        private async static Task CopyDbSetAsync<T>(DbSet<T> source, DbSet<T> target, Action<T> entityHandler = null, params string[] includes)
+        private async static Task CopyDbSetAsync<T>(DbSet<T> source, DbSet<T> target, Action<T> entityHandler = null, Func<T, Task<bool>> containsHandler = null, params string[] includes)
             where T : class
         {
             IQueryable<T> query = source;
@@ -40,10 +41,17 @@ namespace Money
                 query = query.Include(include);
 
             var entities = await query.ToListAsync();
+            Console.WriteLine($"Processing {entities.Count} entities of type {typeof(T).Name}.");
 
             foreach (var entity in entities)
             {
-                if (target.Contains(entity))
+                bool isContained;
+                if (containsHandler != null)
+                    isContained = await containsHandler(entity);
+                else
+                    isContained = target.Contains(entity);
+
+                if (isContained)
                     target.Update(entity);
                 else
                     target.Add(entity);
@@ -58,12 +66,20 @@ namespace Money
 
         private async static Task MigrateApplicationAsync(string sourceConnectionString, string targetConnectionString)
         {
-            using (var source = new AccountContext(new DbContextOptionsBuilder<AccountContext>().UseSqlite(sourceConnectionString).Options, new SchemaOptions<AccountContext>()))
-            using (var target = new AccountContext(new DbContextOptionsBuilder<AccountContext>().UseSqlServer(targetConnectionString).Options, new SchemaOptions<AccountContext>() { Name = "Application" }))
+            Console.WriteLine("Migrate Application.");
+
+            SchemaOptions<AccountContext> sourceSchema = new SchemaOptions<AccountContext>();
+            SchemaOptions<AccountContext> targetSchema = new SchemaOptions<AccountContext>() { Name = "Application" };
+            MigrationWithSchema.SetSchema(sourceSchema);
+            MigrationWithSchema.SetSchema(targetSchema);
+
+            using (var source = new AccountContext(new DbContextOptionsBuilder<AccountContext>().UseSqlite(sourceConnectionString).Options, sourceSchema))
+            using (var target = new AccountContext(new DbContextOptionsBuilder<AccountContext>().UseSqlServer(targetConnectionString).Options, targetSchema))
             {
-                await target.Database.EnsureCreatedAsync();
+                Console.WriteLine("Migrate schema.");
                 await target.Database.MigrateAsync();
 
+                Console.WriteLine("Migrate data.");
                 await CopyDbSetAsync(source, target, c => c.Users);
                 await CopyDbSetAsync(source, target, c => c.UserClaims);
                 await CopyDbSetAsync(source, target, c => c.UserLogins);
@@ -72,39 +88,76 @@ namespace Money
                 await CopyDbSetAsync(source, target, c => c.RoleClaims);
                 await CopyDbSetAsync(source, target, c => c.UserRoles);
 
+                Console.WriteLine("Save changes.");
                 await target.SaveChangesAsync();
+
+                Console.WriteLine("Completed.");
             }
         }
 
         private async static Task MigrateEventSourcingAsync(string sourceConnectionString, string targetConnectionString)
         {
-            using (var source = new EventSourcingContext(new DbContextOptionsBuilder<EventSourcingContext>().UseSqlite(sourceConnectionString).Options, new SchemaOptions<EventSourcingContext>()))
-            using (var target = new EventSourcingContext(new DbContextOptionsBuilder<EventSourcingContext>().UseSqlServer(targetConnectionString).Options, new SchemaOptions<EventSourcingContext>() { Name = "EventSourcing" }))
+            Console.WriteLine("Migrate EventSourcing.");
+
+            SchemaOptions<EventSourcingContext> sourceSchema = new SchemaOptions<EventSourcingContext>();
+            SchemaOptions<EventSourcingContext> targetSchema = new SchemaOptions<EventSourcingContext>() { Name = "EventSourcing" };
+            MigrationWithSchema.SetSchema(sourceSchema);
+            MigrationWithSchema.SetSchema(targetSchema);
+
+            using (var source = new EventSourcingContext(new DbContextOptionsBuilder<EventSourcingContext>().UseSqlite(sourceConnectionString).Options, sourceSchema))
+            using (var target = new EventSourcingContext(new DbContextOptionsBuilder<EventSourcingContext>().UseSqlServer(targetConnectionString).Options, targetSchema))
             {
-                await target.Database.EnsureCreatedAsync();
+                Console.WriteLine("Migrate schema.");
                 await target.Database.MigrateAsync();
 
+                target.Database.OpenConnection();
+                await target.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [EventSourcing].[Event] ON;");
+
+                Console.WriteLine("Migrate data.");
                 await CopyDbSetAsync(source, target, c => c.Events);
 
+                Console.WriteLine("Save changes.");
                 await target.SaveChangesAsync();
+
+                await target.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [EventSourcing].[Event] OFF;");
+                target.Database.CloseConnection();
+
+                Console.WriteLine("Completed.");
             }
         }
 
         private async static Task MigrateReadModelAsync(string sourceConnectionString, string targetConnectionString)
         {
-            using (var source = new ReadModelContext(new DbContextOptionsBuilder<ReadModelContext>().UseSqlite(sourceConnectionString).Options, new SchemaOptions<ReadModelContext>()))
-            using (var target = new ReadModelContext(new DbContextOptionsBuilder<ReadModelContext>().UseSqlServer(targetConnectionString).Options, new SchemaOptions<ReadModelContext>() { Name = "ReadModel" }))
+            Console.WriteLine("Migrate ReadModel.");
+
+            SchemaOptions<ReadModelContext> sourceSchema = new SchemaOptions<ReadModelContext>();
+            SchemaOptions<ReadModelContext> targetSchema = new SchemaOptions<ReadModelContext>() { Name = "ReadModel" };
+            MigrationWithSchema.SetSchema(sourceSchema);
+            MigrationWithSchema.SetSchema(targetSchema);
+
+            using (var source = new ReadModelContext(new DbContextOptionsBuilder<ReadModelContext>().UseSqlite(sourceConnectionString).Options, sourceSchema))
+            using (var target = new ReadModelContext(new DbContextOptionsBuilder<ReadModelContext>().UseSqlServer(targetConnectionString).Options, targetSchema))
             {
-                await target.Database.EnsureCreatedAsync();
+                Console.WriteLine("Migrate schema.");
                 await target.Database.MigrateAsync();
 
-                await CopyDbSetAsync(source, target, c => c.Currencies);
+                target.Database.OpenConnection();
+                await target.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [ReadModel].[ExchangeRates] ON;");
+
+                Console.WriteLine("Migrate data.");
+                await CopyDbSetAsync(source, target, c => c.Currencies, containsHandler: e => target.Currencies.AnyAsync(c => c.UserId == e.UserId && c.UniqueCode == e.UniqueCode));
                 await CopyDbSetAsync(source, target, c => c.Categories);
                 await CopyDbSetAsync(source, target, c => c.ExchangeRates);
                 await CopyDbSetAsync(source, target, c => c.Outcomes);
-                await CopyDbSetAsync(source, target, c => c.OutcomeCategories);
+                await CopyDbSetAsync(source, target, c => c.OutcomeCategories, containsHandler: e => target.OutcomeCategories.AnyAsync(ec => e.CategoryId == ec.CategoryId && e.OutcomeId == ec.OutcomeId));
 
+                Console.WriteLine("Save changes.");
                 await target.SaveChangesAsync();
+
+                await target.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [ReadModel].[ExchangeRates] OFF;");
+                target.Database.CloseConnection();
+
+                Console.WriteLine("Completed.");
             }
         }
     }

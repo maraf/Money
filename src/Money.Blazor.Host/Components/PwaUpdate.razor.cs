f96ca7dc8561877fbc43d2fce33b15005acd9874
Marek Fišera <mara@neptuo.com>
2021-05-20 23:43:05 +0200

    #291 - Fullscreen notification about the available update.

---
+++ src/Money.Blazor.Host/Components/PwaUpdate.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using Money.Events;
+using Neptuo.Commands;
+using Neptuo.Events;
+using Neptuo.Events.Handlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Components
+{
+    public partial class PwaUpdate : IDisposable,
+        IEventHandler<PwaUpdateable>
+    {
+        [Inject]
+        protected IEventHandlerCollection EventHandlers { get; set; }
+
+        [Inject]
+        protected ICommandDispatcher Commands { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected bool IsUpdateable { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            EventHandlers.Add<PwaUpdateable>(this);
+        }
+
+        public void Dispose()
+        {
+            EventHandlers.Remove<PwaUpdateable>(this);
+        }
+
+        Task IEventHandler<PwaUpdateable>.HandleAsync(PwaUpdateable payload)
+        {
+            IsUpdateable = true;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}

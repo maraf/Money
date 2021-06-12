37df1cf1af0712ad2b05b719fc82c8bbf06a856d
Marek Fišera <mara@neptuo.com>
2021-06-12 09:31:18 +0200

    #385 - Reconnect when document becomes visible and we are not connected.

--- src/Money.Blazor.Host/Services/SignalRListener.cs
+++ src/Money.Blazor.Host/Services/SignalRListener.cs
@@ -1,6 +1,7 @@
 ﻿using Money.Events;
 using Neptuo;
 using Neptuo.Events.Handlers;
+using Neptuo.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,14 +11,17 @@ using System.Threading.Tasks;
 
 namespace Money.Services
 {
-    public class SignalRListener : IEventHandler<UserSignedIn>, IEventHandler<UserSignedOut>
+    public class SignalRListener : IEventHandler<UserSignedIn>, IEventHandler<UserSignedOut>, IEventHandler<VisibilityChanged>
     {
         private readonly ApiHubService apiHub;
+        private readonly ILog log;
 
-        public SignalRListener(ApiHubService apiHub)
+        public SignalRListener(ApiHubService apiHub, ILogFactory logFactory)
         {
             Ensure.NotNull(apiHub, "apiHub");
+            Ensure.NotNull(logFactory, "logFactory");
             this.apiHub = apiHub;
+            this.log = logFactory.Scope("SignalRListener");
         }
 
         Task IEventHandler<UserSignedIn>.HandleAsync(UserSignedIn payload) 
@@ -25,5 +29,18 @@ namespace Money.Services
 
         Task IEventHandler<UserSignedOut>.HandleAsync(UserSignedOut payload) 
             => apiHub.StopAsync();
+
+        public Task HandleAsync(VisibilityChanged payload)
+        {
+            log.Debug($"Got event '{nameof(VisibilityChanged)}' is value '{payload.IsVisible}'.");
+
+            if (payload.IsVisible && apiHub.Status != ApiHubStatus.Connected)
+            {
+                log.Debug("Starting connection.");
+                return apiHub.StartAsync();
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }

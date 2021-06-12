d010c284b9b201c64e61a64c7157ee1a00d0fcc6
Marek Fišera <mara@neptuo.com>
2021-06-12 10:30:54 +0200

    #380 - Show release notes when update is available.
    Prepare release notes for blazor-v1.16.0.

--- src/Money.Blazor.Host/wwwroot/service-worker.published.js
+++ src/Money.Blazor.Host/wwwroot/service-worker.published.js
@@ -6,7 +6,7 @@ self.importScripts('./service-worker-assets.js');
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/];
-const offlineAssetsExclude = [/^service-worker\.js$/];
+const offlineAssetsExclude = [/^service-worker\.js$/, /^release-notes\.html$/];
 
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));

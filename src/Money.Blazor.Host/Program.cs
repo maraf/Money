c0c4020987c2d817c7ae6077c20ccf90f0e920cf
Marek Fišera <mara@neptuo.com>
2021-08-02 10:19:31 +0200

    #397 - Drop playing component observers.

--- src/Money.Blazor.Host/Program.cs
+++ src/Money.Blazor.Host/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Blazored.LocalStorage;
 using Blazored.SessionStorage;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -12,11 +10,8 @@ using Money.Components.Bootstrap;
 using Money.Models;
 using Money.Services;
 using Neptuo.Events;
-using Neptuo.Events.Handlers;
 using Neptuo.Exceptions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -92,9 +87,6 @@ namespace Money.UI.Blazor
                 .AddBlazoredSessionStorage();
 
             services
-                .AddTransient<IComponentActivator, ComponentActivator>();
-
-            services
                 .AddSingleton<TemplateService>();
 
             bootstrapTask = new Bootstrap.BootstrapTask(services);
@@ -117,83 +109,6 @@ namespace Money.UI.Blazor
                 .AddValidator(services.GetRequiredService<ApiTokenValidator>());
 
             services.GetRequiredService<VisibilityState>();
-        }
-    }
-
-    class ComponentActivator : IComponentActivator
-    {
-        public IComponent CreateInstance(Type componentType)
-        {
-            Console.WriteLine($"CA: '{componentType.FullName}'.");
-
-            if (componentType.GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEventHandler<>)))
-            {
-                Console.WriteLine($"CA: '{componentType.FullName}' as 'EventAttachedComponent'.");
-                return new EventAttachedComponent() { Type = componentType };
-            }
-            else if(componentType.IsGenericType && componentType.GetGenericTypeDefinition() == typeof(Wrapped<>))
-            {
-                componentType = componentType.GetGenericArguments()[0];
-            }
-
-            return (IComponent)Activator.CreateInstance(componentType);
-        }
-    }
-
-    class Wrapped<T> : IComponent
-    {
-        public void Attach(RenderHandle renderHandle)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetParametersAsync(ParameterView parameters)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    class EventAttachedComponent : IComponent
-    {
-        private RenderHandle _renderHandle;
-        private RenderFragment _cachedRenderFragment;
-
-        public EventAttachedComponent()
-        {
-            _cachedRenderFragment = Render;
-        }
-
-        public Type Type { get; set; }
-        public IDictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
-
-        public void Attach(RenderHandle renderHandle)
-        {
-            _renderHandle = renderHandle;
-        }
-
-        public Task SetParametersAsync(ParameterView parameters)
-        {
-            Parameters.Clear();
-            foreach (var entry in parameters)
-                Parameters[entry.Name] = entry.Value;
-
-            _renderHandle.Render(_cachedRenderFragment);
-            return Task.CompletedTask;
-        }
-
-        void Render(RenderTreeBuilder builder)
-        {
-            builder.OpenComponent(0, typeof(Wrapped<>).MakeGenericType(Type));
-
-            if (Parameters != null)
-            {
-                foreach (var entry in Parameters)
-                {
-                    builder.AddAttribute(1, entry.Key, entry.Value);
-                }
-            }
-
-            builder.CloseComponent();
         }
     }
 }

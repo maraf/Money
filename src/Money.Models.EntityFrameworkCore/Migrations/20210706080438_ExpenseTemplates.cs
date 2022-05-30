d29fb89a79f10fd3c828cd87f63702d41a10fcf0
Marek Fišera <mara@neptuo.com>
2022-05-30 15:13:35 +0200

    #397 - More fixes to the migration.

--- src/Money.Models.EntityFrameworkCore/Migrations/20210706080438_ExpenseTemplates.cs
+++ src/Money.Models.EntityFrameworkCore/Migrations/20210706080438_ExpenseTemplates.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Money.EntityFrameworkCore.Migrations;
 
 namespace Money.Models.Migrations
 {
-    public partial class ExpenseTemplates : Migration
+    public partial class ExpenseTemplates : MigrationWithSchema<ReadModelContext>
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
                 name: "ExpenseTemplates",
+                schema: Schema.Name,
                 columns: table => new
                 {
                     Id = table.Column<Guid>(nullable: false),
-                    UserId = table.Column<string>(type: "TEXT", nullable: true),
-                    Description = table.Column<string>(type: "TEXT", nullable: true),
-                    Amount = table.Column<decimal>(type: "TEXT", nullable: true),
-                    Currency = table.Column<string>(type: "TEXT", nullable: true),
-                    CategoryId = table.Column<Guid>(type: "TEXT", nullable: true)
+                    UserId = table.Column<string>(maxLength: 36, nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: true),
+                    Currency = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<Guid>(nullable: true)
                 },
                 constraints: table =>
                 {
@@ -27,7 +29,8 @@ namespace Money.Models.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "ExpenseTemplates");
+                name: "ExpenseTemplates",
+                schema: Schema.Name);
         }
     }
 }

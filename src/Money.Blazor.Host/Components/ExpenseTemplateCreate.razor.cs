5ba4bb1fe487f1d6a27a63abb2db2a84c0ef6b7a
Marek Fišera <mara@neptuo.com>
2022-06-13 15:59:48 +0200

    #417 - Create expense templates.

---
+++ src/Money.Blazor.Host/Components/ExpenseTemplateCreate.razor.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Components;
+using Money.Commands;
+using Money.Models;
+using Money.Models.Queries;
+using Money.Services;
+using Neptuo;
+using Neptuo.Commands;
+using Neptuo.Logging;
+using Neptuo.Models.Keys;
+using Neptuo.Queries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Components
+{
+    public partial class ExpenseTemplateCreate
+    {
+        [Inject]
+        protected ICommandDispatcher Commands { get; set; }
+
+        [Inject]
+        protected IQueryDispatcher Queries { get; set; }
+
+        [Inject]
+        internal ILog<ExpenseTemplateCreate> Log { get; set; }
+
+        protected List<string> ErrorMessages { get; } = new List<string>();
+
+        protected List<CategoryModel> Categories { get; private set; }
+        protected List<CurrencyModel> Currencies { get; private set; }
+
+        protected Confirm PrerequisitesConfirm { get; set; }
+
+        [Parameter]
+        public decimal Amount { get; set; }
+
+        [Parameter]
+        public string Currency { get; set; }
+
+        [Parameter]
+        public string Description { get; set; }
+
+        [Parameter]
+        public IKey CategoryKey { get; set; }
+
+        protected void OnSaveClick()
+        {
+            if (Validate())
+            {
+                Execute();
+                Modal.Hide();
+            }
+        }
+
+        private bool Validate()
+        {
+            Log.Debug($"Expense: Amount: {Amount}, Currency: {Currency}, Category: {CategoryKey}.");
+
+            ErrorMessages.Clear();
+
+            if ((Amount == 0 && Currency != null) || (Amount != 0 && Currency == null))
+                ErrorMessages.Add("Amount and currency must be provided both or none");
+
+            if (Amount != 0)
+                Validator.AddOutcomeAmount(ErrorMessages, Amount);
+
+            Log.Debug($"Expense: Validation: {string.Join(", ", ErrorMessages)}.");
+            return ErrorMessages.Count == 0;
+        }
+
+        private async void Execute()
+        {
+            var price = Amount != 0
+                ? new Price(Amount, Currency)
+                : null;
+
+            await Commands.HandleAsync(new CreateExpenseTemplate(price, Description, CategoryKey));
+
+            Amount = 0;
+            CategoryKey = null;
+            Description = null;
+            StateHasChanged();
+        }
+
+        public async void Show(IKey categoryKey)
+        {
+            Ensure.NotNull(categoryKey, "categoryKey");
+            CategoryKey = categoryKey;
+
+            Categories = await Queries.QueryAsync(new ListAllCategory());
+            Currencies = await Queries.QueryAsync(new ListAllCurrency());
+            Currency = await Queries.QueryAsync(new FindCurrencyDefault());
+
+            if (Currencies == null || Currencies.Count == 0 || Categories == null || Categories.Count == 0)
+                PrerequisitesConfirm.Show();
+            else
+                base.Show();
+
+            StateHasChanged();
+        }
+
+        public override void Show()
+            => Show(KeyFactory.Empty(typeof(Category)));
+    }
+}
--- src/Money.Blazor.Host/Pages/ExpenseTemplates.razor.cs
+++ src/Money.Blazor.Host/Pages/ExpenseTemplates.razor.cs
@@ -10,6 +10,7 @@ using Neptuo.Events;
 using Neptuo.Events.Handlers;
 using Neptuo.Logging;
 using Neptuo.Queries;
+using Neptuo.Queries.Handlers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,8 +20,49 @@ using System.Threading.Tasks;
 
 namespace Money.Pages
 {
-    public partial class ExpenseTemplates
+    public partial class ExpenseTemplates : IDisposable, IEventHandler<ExpenseTemplateCreated>, IEventHandler<ExpenseTemplateDeleted>
     {
+        [Inject]
+        protected IEventHandlerCollection EventHandlers { get; set; }
 
+        [Inject]
+        protected IQueryDispatcher Queries { get; set; }
+
+        protected ExpenseTemplateCreate Modal { get; set; }
+        protected List<ExpenseTemplateModel> Models { get; } = new List<ExpenseTemplateModel>();
+
+        protected async override Task OnInitializedAsync()
+        {
+            EventHandlers
+                .Add<ExpenseTemplateCreated>(this)
+                .Add<ExpenseTemplateDeleted>(this);
+
+            await base.OnInitializedAsync();
+
+            await ReloadAsync();
+        }
+
+        private async Task ReloadAsync()
+        {
+            Models.Clear();
+            Models.AddRange(await Queries.QueryAsync(new ListAllExpenseTemplate()));
+            StateHasChanged();
+        }
+
+        public void Dispose() => EventHandlers
+                .Remove<ExpenseTemplateCreated>(this)
+                .Remove<ExpenseTemplateDeleted>(this);
+
+        public Task HandleAsync(ExpenseTemplateCreated payload)
+        {
+            _ = ReloadAsync();
+            return Task.CompletedTask;
+        }
+
+        public Task HandleAsync(ExpenseTemplateDeleted payload)
+        {
+            _ = ReloadAsync();
+            return Task.CompletedTask;
+        }
     }
 }

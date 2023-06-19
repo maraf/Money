67a757273d9186ab5efebae6a4386bd7cdebd95a
Marek Fišera <mara@neptuo.com>
2023-06-19 18:33:28 +0200

    #470 - Command handlers.

--- src/Money/Commands/Handlers/ExpenseTemplateHandler.cs
+++ src/Money/Commands/Handlers/ExpenseTemplateHandler.cs
@@ -14,6 +14,10 @@ namespace Money.Commands.Handlers
 {
     public class ExpenseTemplateHandler : AggregateRootCommandHandler<ExpenseTemplate>,
         ICommandHandler<Envelope<CreateExpenseTemplate>>,
+        ICommandHandler<Envelope<ChangeExpenseTemplateAmount>>,
+        ICommandHandler<Envelope<ChangeExpenseTemplateDescription>>,
+        ICommandHandler<Envelope<ChangeExpenseTemplateWhen>>,
+        ICommandHandler<Envelope<ChangeExpenseTemplateFixed>>,
         ICommandHandler<Envelope<DeleteExpenseTemplate>>
     {
         public ExpenseTemplateHandler(IFactory<IRepository<ExpenseTemplate, IKey>> repositoryFactory)
@@ -29,5 +33,13 @@ namespace Money.Commands.Handlers
         });
 
         public Task HandleAsync(Envelope<DeleteExpenseTemplate> envelope) => WithCommand(envelope.Body.Key).Execute(envelope.Body.ExpenseTemplateKey, envelope, model => model.Delete());
+
+        public Task HandleAsync(Envelope<ChangeExpenseTemplateAmount> envelope) => WithCommand(envelope.Body.Key).Execute(envelope.Body.ExpenseTemplateKey, envelope, model => model.ChangeAmount(envelope.Body.Amount));
+
+        public Task HandleAsync(Envelope<ChangeExpenseTemplateDescription> envelope) => WithCommand(envelope.Body.Key).Execute(envelope.Body.ExpenseTemplateKey, envelope, model => model.ChangeDescription(envelope.Body.Description));
+
+        public Task HandleAsync(Envelope<ChangeExpenseTemplateWhen> envelope) => WithCommand(envelope.Body.Key).Execute(envelope.Body.ExpenseTemplateKey, envelope, model => model.ChangeWhen(envelope.Body.When));
+
+        public Task HandleAsync(Envelope<ChangeExpenseTemplateFixed> envelope) => WithCommand(envelope.Body.Key).Execute(envelope.Body.ExpenseTemplateKey, envelope, model => model.ChangeFixed(envelope.Body.IsFixed));
     }
 }

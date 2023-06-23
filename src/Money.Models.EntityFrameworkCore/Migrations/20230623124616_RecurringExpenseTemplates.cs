9eb5806eed12fd35144e5115a070fa9b45f6c429
Marek Fišera <mara@neptuo.com>
2023-06-23 15:02:27 +0200

    #483 - Migration for expense template recurrence.

---
+++ src/Money.Models.EntityFrameworkCore/Migrations/20230623124616_RecurringExpenseTemplates.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Money.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Money.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecurringExpenseTemplates : MigrationWithSchema<ReadModelContext>
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DayInPeriod",
+                table: "ExpenseTemplates",
+                schema: Schema.Name,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Period",
+                table: "ExpenseTemplates",
+                schema: Schema.Name,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayInPeriod",
+                table: "ExpenseTemplates",
+                schema: Schema.Name);
+
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "ExpenseTemplates",
+                schema: Schema.Name);
+        }
+    }
+}

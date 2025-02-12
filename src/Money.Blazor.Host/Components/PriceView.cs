0e3361322de2190645fe3e14e3dc665dd24dc820
Marek Fišera <mara@neptuo.com>
2025-02-12 13:04:22 +0100

    #551 - Component for displaying price with nowrap ensured

---
+++ src/Money.Blazor.Host/Components/PriceView.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Money.Services;
+
+namespace Money.Components;
+
+public class PriceView : ComponentBase
+{
+    [Inject]
+    protected CurrencyFormatterFactory FormatterFactory { get; set; }
+    private CurrencyFormatter formatter;
+
+    [Parameter]
+    public Price Value { get; set; }
+
+    [Parameter]
+    public CurrencyFormatter.FormatZero Zero { get; set; } = CurrencyFormatter.FormatZero.Zero;
+
+    [Parameter]
+    public bool ApplyUserDigits { get; set; } = true;
+
+    [Parameter]
+    public bool ApplyPlusForPositiveNumbers { get; set; } = false;
+
+    [Parameter]
+    public string TagName { get; set; } = "span";
+
+    [Parameter]
+    public string CssClass { get; set; }
+
+    [Parameter]
+    public string CssStyle { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        formatter = await FormatterFactory.CreateAsync();
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        int index = 0;
+        builder.OpenElement(index++, TagName);
+        builder.AddAttribute(index++, "class", $"{CssClass} text-nowrap");
+        
+        if (!String.IsNullOrEmpty(CssStyle))
+            builder.AddAttribute(index++, "style", CssStyle);
+
+        builder.AddContent(index++, formatter.Format(Value, Zero, ApplyUserDigits, ApplyPlusForPositiveNumbers));
+        builder.CloseElement();
+    }
+}

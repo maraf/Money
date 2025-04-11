b9b79c1d4c2424e87a7c5bf73ac855911ba34b0b
Marek Fišera <mara@neptuo.com>
2025-04-11 09:05:37 +0200

    #567 - Replace property injection with constructor injection

--- src/Money.Blazor.Host/Pages/OverviewMonth.cs
+++ src/Money.Blazor.Host/Pages/OverviewMonth.cs
@@ -17,12 +17,12 @@ using System.Threading.Tasks;
 
 namespace Money.Pages;
 
-    [Route("/outcomes/{Year:int}/{Month:int}")]
-    [Route("/overview/{Year:int}/{Month:int}")]
-    [Route("/overview/{Year:int}/{Month:int}/{CategoryGuid:guid}")]
-    [Route("/{Year:int}/{Month:int}/overview")]
-    [Route("/{Year:int}/{Month:int}/overview/{CategoryGuid:guid}")]
-    [Authorize]
+[Route("/outcomes/{Year:int}/{Month:int}")]
+[Route("/overview/{Year:int}/{Month:int}")]
+[Route("/overview/{Year:int}/{Month:int}/{CategoryGuid:guid}")]
+[Route("/{Year:int}/{Month:int}/overview")]
+[Route("/{Year:int}/{Month:int}/overview/{CategoryGuid:guid}")]
+[Authorize]
 public partial class OverviewMonth(
     IEventHandlerCollection EventHandlers,
     IQueryDispatcher Queries,
@@ -37,61 +37,61 @@ public partial class OverviewMonth(
     Log,
     "List of each single expense in selected month"
 )
+{
+    [Parameter]
+    public int? Year { get; set; }
+
+    [Parameter]
+    public int? Month { get; set; }
+
+    [Parameter]
+    public Guid? CategoryGuid { get; set; }
+
+    protected override void ClearPreviousParameters()
     {
-        [Parameter]
-        public int? Year { get; set; }
+        Year = null;
+        Month = null;
+    }
 
-        [Parameter]
-        public int? Month { get; set; }
+    protected override MonthModel CreateSelectedItemFromParameters()
+        => new MonthModel(Year.Value, Month.Value);
 
-        [Parameter]
-        public Guid? CategoryGuid { get; set; }
+    protected override IKey CreateSelectedCategoryFromParameters()
+        => CategoryGuid != null ? GuidKey.Create(CategoryGuid.Value, KeyFactory.Empty(typeof(Category)).Type) : KeyFactory.Empty(typeof(Category));
 
-        protected override void ClearPreviousParameters()
-        {
-            Year = null;
-            Month = null;
-        }
+    protected override IQuery<List<OutcomeOverviewModel>> CreateItemsQuery(int pageIndex)
+        => ListMonthOutcomeFromCategory.Version2(CategoryKey, SelectedPeriod, SortDescriptor, pageIndex);
 
-        protected override MonthModel CreateSelectedItemFromParameters()
-            => new MonthModel(Year.Value, Month.Value);
+    protected override bool IsContained(DateTime when)
+        => SelectedPeriod == when;
 
-        protected override IKey CreateSelectedCategoryFromParameters()
-            => CategoryGuid != null ? GuidKey.Create(CategoryGuid.Value, KeyFactory.Empty(typeof(Category)).Type) : KeyFactory.Empty(typeof(Category));
+    protected override string ListIncomeUrl()
+    {
+        if (CategoryKey.IsEmpty)
+            return Navigator.UrlOverviewIncomes(SelectedPeriod);
 
-        protected override IQuery<List<OutcomeOverviewModel>> CreateItemsQuery(int pageIndex)
-            => ListMonthOutcomeFromCategory.Version2(CategoryKey, SelectedPeriod, SortDescriptor, pageIndex);
+        return null;
+    }
 
-        protected override bool IsContained(DateTime when)
-            => SelectedPeriod == when;
+    protected override string ChecklistUrl()
+    {
+        if (CategoryKey.IsEmpty)
+            return Navigator.UrlChecklist(SelectedPeriod);
 
-        protected override string ListIncomeUrl()
-        {
-            if (CategoryKey.IsEmpty)
-                return Navigator.UrlOverviewIncomes(SelectedPeriod);
+        return null;
+    }
 
+    protected override (string title, string url)? TrendsTitleUrl()
+    {
+        if (CategoryKey.IsEmpty)
             return null;
-        }
 
-        protected override string ChecklistUrl()
-        {
-            if (CategoryKey.IsEmpty)
-                return Navigator.UrlChecklist(SelectedPeriod);
+        return ("Month trends", Navigator.UrlTrends(new YearModel(SelectedPeriod.Year), CategoryKey));
+    }
 
-            return null;
-        }
+    protected override void OpenNextPeriod() 
+        => Navigator.OpenOverview(SelectedPeriod + 1);
 
-        protected override (string title, string url)? TrendsTitleUrl()
-        {
-            if (CategoryKey.IsEmpty)
-                return null;
-
-            return ("Month trends", Navigator.UrlTrends(new YearModel(SelectedPeriod.Year), CategoryKey));
-        }
-
-        protected override void OpenNextPeriod() 
-            => Navigator.OpenOverview(SelectedPeriod + 1);
-
-        protected override void OpenPrevPeriod()
-            => Navigator.OpenOverview(SelectedPeriod - 1);
+    protected override void OpenPrevPeriod()
+        => Navigator.OpenOverview(SelectedPeriod - 1);
 }
